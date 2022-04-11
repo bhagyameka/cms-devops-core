068321a369854dbb177ae2eb46352f7043ecc55d
bhagyameka <bhagyakakumanu@gmail.com>
2022-04-11 15:13:11 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -11,7 +11,7 @@ def call(String branch = 'null') {
 					echo "You have chosen branch $BRANCH_NAME"
 					println "$BRANCH_NAME"
 					script{
-						if ("$BRANCH_NAME" == 'master' && "$BRANCH_NAME" == 'develop' && "$BRANCH_NAME" == 'integrate'){
+						if ("$BRANCH_NAME" == 'master' || "$BRANCH_NAME" == 'develop' || "$BRANCH_NAME" == 'integrate'){
 					git branch: "$BRANCH_NAME", url: "https://github.com/bhagyameka/calculator-scriptsrepo.git", credentialsId: 'c6947d68-906d-4126-a88a-d93c8d4a1ec8'
 				          }
 						else 
