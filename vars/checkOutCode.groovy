2840a15e4b943782276c3ad28921e6905cb97807
bhagyameka <bhagyakakumanu@gmail.com>
2022-04-22 14:21:56 +0530

    Update checkOutCode.groovy

--- vars/checkOutCode.groovy
+++ vars/checkOutCode.groovy
@@ -1,26 +1,22 @@
 static def call(String branch = 'null') {
-	pipeline {
+	
 		environment {
 			BRANCH_NAME = "${branch}"
 		}
-		agent {label 'slave2'}
-		stages {
-			stage('Source Code from SCM') {
-				steps {
-					echo "You have chosen branch $BRANCH_NAME"
-					println "$BRANCH_NAME"
-					script{
-						if ( "$BRANCH_NAME" == 'develop'){
-					git branch: "$BRANCH_NAME", url: "https://github.com/bhagyameka/calculator-scriptsrepo.git", credentialsId: 'c6947d68-906d-4126-a88a-d93c8d4a1ec8'
-				          }
-						else 
-						{
-					         echo " $BRANCH_NAME branch is not allowed for building "
-						 sh 'exit 1'
-						}
-					}
-				}
+		echo "You have chosen branch $BRANCH_NAME"
+		println "$BRANCH_NAME"
+		script{
+		   if ( "$BRANCH_NAME" == 'develop'){
+			git branch: "$BRANCH_NAME", url: "https://github.com/bhagyameka/calculator-scriptsrepo.git", credentialsId: 'c6947d68-906d-4126-a88a-d93c8d4a1ec8'
+		      }
+		   else 
+			{
+		    echo " $BRANCH_NAME branch is not allowed for building "
+		    sh 'exit 1'
 			}
-    }
-  }
+		}
+	    }
+	
+    
+  
   
