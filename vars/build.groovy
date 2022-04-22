2dde2a312a57d29e0584f99be020de5f67a93d34
bhagyameka <bhagyakakumanu@gmail.com>
2022-04-22 14:31:29 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,25 +1,10 @@
-def call(String branch = 'null') {
-	echo "This is shared library build: ${branch}"
+import checkOutCode
 	pipeline {
-		environment {
-			BRANCH_NAME = "${branch}"
-		}
 		agent {label 'slave2'}
 		stages {
 			stage('Source Code from SCM') {
 				steps {
-					echo "You have chosen branch $BRANCH_NAME"
-					println "$BRANCH_NAME"
-					script{
-						if ("$BRANCH_NAME" == 'master' || "$BRANCH_NAME" == 'develop' || "$BRANCH_NAME" == 'integrate'){
-					git branch: "$BRANCH_NAME", url: "https://github.com/bhagyameka/calculator-scriptsrepo.git", credentialsId: 'c6947d68-906d-4126-a88a-d93c8d4a1ec8'
-				          }
-						else 
-						{
-					         echo " No such branch : $BRANCH_NAME "
-						 sh 'exit 1'
-						}
-					}
+					codeCheckOut.call("$BRANCH_NAME")
 				}
 			}
 		        stage('build using maven') {
