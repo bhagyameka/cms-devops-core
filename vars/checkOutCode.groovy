636a047bc3f089dde5a5e63457c8fd8d6821cdd9
bhagyameka <bhagyakakumanu@gmail.com>
2022-04-22 15:05:09 +0530

    Update checkOutCode.groovy

--- vars/checkOutCode.groovy
+++ vars/checkOutCode.groovy
@@ -4,7 +4,7 @@ static def call(String branch = 'null') {
 			BRANCH_NAME = "${branch}"
 		}
 		echo "You have chosen branch:"
-		println "$BRANCH_NAME"
+	        println "${BRANCH_NAME}"
 		script{
 		   if ( "$BRANCH_NAME" == 'develop'){
 			git branch: "$BRANCH_NAME", url: "https://github.com/bhagyameka/calculator-scriptsrepo.git", credentialsId: 'c6947d68-906d-4126-a88a-d93c8d4a1ec8'
