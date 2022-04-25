1dd7e15df477ce3670745f86888e07f06b04ca09
bhagyameka <bhagyakakumanu@gmail.com>
2022-04-25 15:48:13 +0530

    Update checkOutCode.groovy

--- vars/checkOutCode.groovy
+++ vars/checkOutCode.groovy
@@ -1,22 +1,31 @@
 
 def fromgithub (String branch = 'null') {
-	
-		environment {
+pipeline {
+	environment {
 			BRANCH_NAME = "${branch}"
 		}
-		echo "You have chosen branch:"
-	        println "${branch}"
-		script{
-		   if ( "$branch" == 'develop'){
-			git branch: "$branch", url: "https://github.com/bhagyameka/calculator-scriptsrepo.git", credentialsId: 'c6947d68-906d-4126-a88a-d93c8d4a1ec8'
-		      }
-		   else 
-			{
-		    echo " $branch branch is not allowed for building "
-		    sh 'exit 1'
+		agent {label 'slave2'}
+		stages {
+			stage('Source Code from SCM') {
+				steps {
+					echo "You have chosen branch:"
+	                                println "${branch}"
+					script{
+					//checkOutCode.fromgithub("develop")
+					 if ( "$branch" == 'develop'){
+			                    git branch: "$branch", url: "https://github.com/bhagyameka/calculator-scriptsrepo.git", credentialsId: 'c6947d68-906d-4126-a88a-d93c8d4a1ec8'
+		                           }
+		                        else 
+			                  {
+		                           echo " $branch branch is not allowed for building "
+		                           sh 'exit 1'
+			                  }	
+				}
 			}
+			}			  
 		}
 	    }
+}
     
   
   
