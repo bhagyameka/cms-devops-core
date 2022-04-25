42db62157df9fbf07245c2515dd0d910f26c51be
bhagyameka <bhagyakakumanu@gmail.com>
2022-04-25 16:28:06 +0530

    Update checkOutCode.groovy

--- vars/checkOutCode.groovy
+++ vars/checkOutCode.groovy
@@ -1,9 +1,9 @@
 
 def call(String branch = 'null') {
-
+ 
 					echo "You have chosen branch:"
 	                                println "${branch}"
-					script{
+					
 					
 					 if ( "$branch" == 'develop'){
 			                    git branch: "$branch", url: "https://github.com/bhagyameka/calculator-scriptsrepo.git", credentialsId: 'c6947d68-906d-4126-a88a-d93c8d4a1ec8'
@@ -13,7 +13,7 @@ def call(String branch = 'null') {
 		                           echo " $branch branch is not allowed for building "
 		                           sh 'exit 1'
 			                  }	
-				}
+				
 			}
 
   
