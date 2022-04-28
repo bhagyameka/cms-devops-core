f72dade83c62dea6c1f6d03ffe2806d8a10e28d3
bhagyameka <bhagyakakumanu@gmail.com>
2022-04-28 14:53:12 +0530

    Update checkOutCode.groovy

--- vars/checkOutCode.groovy
+++ vars/checkOutCode.groovy
@@ -1,20 +1,9 @@
 
 def fromgithub(String branch = 'null') {
- 
 					echo "You have chosen branch:"
 	                                println "${branch}"
-					
-					
-					 if ( "$branch" == 'develop'){
-			                    git branch: "$branch", url: "https://github.com/bhagyameka/calculator-cms.git", credentialsId: 'c6947d68-906d-4126-a88a-d93c8d4a1ec8'
-		                           }
-		                        else 
-			                  {
-		                           echo " $branch branch is not allowed for building "
-		                           sh 'exit 1'
-			                  }	
-				
-			}
+					git branch: "$branch", url: "https://github.com/bhagyameka/calculator-cms.git", credentialsId: 'c6947d68-906d-4126-a88a-d93c8d4a1ec8'
+		                        }
 
   def mavenbuild() {
  
