d54110f3fba4426aab186ba52ddcff8bcf737e9b
bhagyameka <bhagyakakumanu@gmail.com>
2022-05-02 12:08:11 +0530

    Update buildApp.groovy

--- vars/buildApp.groovy
+++ vars/buildApp.groovy
@@ -1,66 +1,16 @@
-def call(String br = 'null'){
-pipeline {
-	environment {
-			BN = "${br}"
-		}
-		agent {label 'slave2'}
-		stages {
-			stage('Source Code from SCM') {
-				steps {
-					echo "You have chosen branch:"
-	                                println "$BN"
-					script{
-					echo " You are on stage Source Code from SCM "	
-						if ("$BN" == 'develop'){
-					checkOutCode.github("$BN")
-						}
-						else{
-							echo " Build on $BN branch is not allowed"
-							sh 'exit 1'
-						}
-				}
-			}
-			}
-		        stage('build using maven') {
-				steps {
-					
-					echo "building using maven"
-					//sh 'mvn --version'
-					//sh 'mvn clean package'
-					script{
-			                checkOutCode.mavenbuild()
-					}
-					}	
-			}
-			stage('unit testing') {
-                               steps {
-       			       echo '*******unit testing starts*******'
-                               junit skipPublishingChecks: true, testResults: '**/target/surefire-reports/TEST-*.xml'   
-                               echo '*******unit testing ends*******'
-                                     }
-                               }
-
-stage ('Test using CheckMarX') {
-   
-    steps {
-        echo '*******CheckMarX start*******'
-        
-   
-        echo '*******CheckMarX end*******'
-    }
-   
+def SourcecodeCheckout(String br = 'null'){
+  echo "You have chosen branch:"
+  println "$br"
+  checkOutCode.github("$br")
 }
-
-
-stage ('Upload to Jfrog') {
-     steps {
-         echo '*******upload to JFrog start*******'
-      
-         echo '*******upload to JFrog End*******'
-     }
+def maven(){
+  echo "building using maven"
+  checkOutCode.mavenbuild()
 }
-		}
-		
-	}
+def unitTesting(){
+	  echo '*******unit testing starts*******'
+          junit skipPublishingChecks: true, testResults: '**/target/surefire-reports/TEST-*.xml'   
+           echo '*******unit testing ends*******'
+           }
+                           
 
-}
